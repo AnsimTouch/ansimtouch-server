be7bab4769cb8690ff350ab46a0a40152dc03e3d
socury <socury1104@gmail.com>
2025-01-10 14:26:46 +0900

    docs: swagger 주석

--- src/main/kotlin/com/ansimtouchserver/domain/attendance/controller/AttendanceController.kt
+++ src/main/kotlin/com/ansimtouchserver/domain/attendance/controller/AttendanceController.kt
@@ -3,18 +3,23 @@ package com.ansimtouchserver.domain.attendance.controller
 import com.ansimtouchserver.domain.attendance.dto.AttendanceDTO
 import com.ansimtouchserver.domain.attendance.service.AttendanceService
 import com.ansimtouchserver.global.dto.BaseResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
 
+@Tag(name = "Attendance", description = "출석체크")
 @RestController
 @RequestMapping("/attendance")
 class AttendanceController (
     private val attendanceService: AttendanceService,
 ) {
+    @Operation(summary = "출석체크 하기")
     @PostMapping("/mark/{userId}")
     fun markAttendance(@PathVariable userId: Long): BaseResponse<Unit> {
         return attendanceService.markAttendance(userId)
     }
 
+    @Operation(summary = "출석체크 했는지 조회")
     @GetMapping("/status/{userId}")
     fun getAttendanceStatus(@PathVariable userId: Long): AttendanceDTO {
         return attendanceService.getAttendanceStatus(userId)
