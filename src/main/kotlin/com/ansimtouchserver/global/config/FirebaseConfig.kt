7c968da4bab153b3ba4de19cb370a7be5c6ce258
socury <socury1104@gmail.com>
2025-01-10 16:06:51 +0900

    fix: firebase 연결 오류

--- src/main/kotlin/com/ansimtouchserver/global/config/FirebaseConfig.kt
+++ src/main/kotlin/com/ansimtouchserver/global/config/FirebaseConfig.kt
@@ -4,22 +4,32 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.messaging.FirebaseMessaging
+import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
+import org.hibernate.query.sqm.tree.SqmNode.log
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.io.ByteArrayInputStream
 
-
 @Configuration
-class FirebaseConfig (
+class FirebaseConfig {
     @Value("\${firebase.json-key}")
-    private val firebaseJsonKey: String
-) {
+    private lateinit var firebaseJsonKey: String
+
     @Bean
     fun firebaseMessaging(): FirebaseMessaging {
+        try {
+            Gson().fromJson(firebaseJsonKey, Map::class.java)
+        } catch (e: JsonSyntaxException) {
+            log.error(firebaseJsonKey)
+            throw IllegalArgumentException("Firebase JSON configuration is malformed", e)
+        }
+
         val options = FirebaseOptions.builder()
             .setCredentials(GoogleCredentials.fromStream(ByteArrayInputStream(firebaseJsonKey.toByteArray())))
             .build()
+
         FirebaseApp.initializeApp(options)
         return FirebaseMessaging.getInstance()
     }
