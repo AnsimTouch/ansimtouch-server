e1b9661780e93ad3e207067efbf4ecafe19ed6e8
socury <socury1104@gmail.com>
2025-01-10 15:14:02 +0900

    feat: firebase 연결

--- src/main/kotlin/com/ansimtouchserver/global/config/FirebaseConfig.kt
+++ src/main/kotlin/com/ansimtouchserver/global/config/FirebaseConfig.kt
@@ -4,16 +4,21 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.messaging.FirebaseMessaging
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.io.FileInputStream
+import java.io.ByteArrayInputStream
+
 
 @Configuration
-class FirebaseConfig {
+class FirebaseConfig (
+    @Value("\${firebase.json-key}")
+    private val firebaseJsonKey: String
+) {
     @Bean
     fun firebaseMessaging(): FirebaseMessaging {
         val options = FirebaseOptions.builder()
-            .setCredentials(GoogleCredentials.fromStream(FileInputStream("ansimtouch-e73c0-firebase-adminsdk-th6cu-d47e7c4a34.json")))
+            .setCredentials(GoogleCredentials.fromStream(ByteArrayInputStream(firebaseJsonKey.toByteArray())))
             .build()
         FirebaseApp.initializeApp(options)
         return FirebaseMessaging.getInstance()
