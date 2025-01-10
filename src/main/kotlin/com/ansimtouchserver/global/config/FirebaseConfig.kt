4bfdfefe69d22c4b47229e2eadb3c6d19ada5e00
socury <socury1104@gmail.com>
2025-01-10 16:33:22 +0900

    fix: firebase 연결 오류

--- src/main/kotlin/com/ansimtouchserver/global/config/FirebaseConfig.kt
+++ src/main/kotlin/com/ansimtouchserver/global/config/FirebaseConfig.kt
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.messaging.FirebaseMessaging
+import org.hibernate.query.sqm.tree.SqmNode.log
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -16,6 +17,7 @@ class FirebaseConfig {
 
     @Bean
     fun firebaseMessaging(): FirebaseMessaging {
+        log.info(firebaseJsonKey)
         val options = FirebaseOptions.builder()
             .setCredentials(GoogleCredentials.fromStream(ByteArrayInputStream(firebaseJsonKey.toByteArray())))
             .build()
