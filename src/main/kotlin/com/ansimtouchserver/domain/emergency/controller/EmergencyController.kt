1059d907d35b2529f411c843eb8529b191484a37
socury <socury1104@gmail.com>
2025-01-10 20:42:25 +0900

    docs: swagger 주석 추가

--- src/main/kotlin/com/ansimtouchserver/domain/emergency/controller/EmergencyController.kt
+++ src/main/kotlin/com/ansimtouchserver/domain/emergency/controller/EmergencyController.kt
@@ -5,17 +5,21 @@ import com.ansimtouchserver.domain.user.repository.UserRepository
 import com.ansimtouchserver.global.dto.BaseResponse
 import com.ansimtouchserver.global.exception.CustomException
 import com.ansimtouchserver.global.util.NotificationService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "Emergency", description = "긴급호출 알림 발송")
 @RestController
 @RequestMapping
 class EmergencyController(
     private val notificationService: NotificationService,
     private val userRepository: UserRepository
 ) {
+    @Operation(summary = "긴급호출")
     @PostMapping("/emergency")
     fun sendEmergencyNotification(@RequestParam userId: Long): BaseResponse<Unit> {
         val user = userRepository.findById(userId).orElseThrow{CustomException(UserErrorCode.USER_NOT_FOUND)}
